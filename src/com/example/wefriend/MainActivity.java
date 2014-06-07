b12999d2393f763a573d1ea8e58c1b3797fe8011
BlackStone-XF <15274581491@163.com>
2014-06-07 13:38:40 +0800

    test

--- src/com/example/wefriend/MainActivity.java
+++ src/com/example/wefriend/MainActivity.java
@@ -2,12 +2,6 @@ package com.example.wefriend;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
 
