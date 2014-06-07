dcf4e7d9502a5004bbbc6cc42a6426bdb3dd12eb
BlackStone-XF <15274581491@163.com>
2014-06-07 13:43:21 +0800

    show

--- src/com/example/wefriend/MainActivity.java
+++ src/com/example/wefriend/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wefriend;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Toast.makeText(getApplicationContext(), "WelCome to WeFriend1.0.0!!!", 500).show();
 	}
 }
